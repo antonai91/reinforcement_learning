58140fe094df7f7fbba295cd34295666ad0a70e6
antonai <antonio.lisi91@gmail.com>
2021-01-21 19:50:17 +0100

    a2c clean

---
+++ a2c_clean/src/config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Name of the Gym environment for the agent to learn & play
+ENV_NAME = 'PongDeterministic-v4'
+
+HIDDEN = 1024
+
+PATH_SAVE_MODEL = "../model"
+
+PATH_LOAD_MODEL = "../model/save_agent_202101121017_00080000"
+
+CONFIG_WANDB = dict (
+  learning_rate = 0.007,
+  batch_size = 64,
+  architecture = "a2c",
+  infra = "Ubuntu"
+)
---
+++ a2c_clean/src/process_image.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+# This function can resize to any shape, but was built to resize to 84x84
+def process_image(image, shape=(84, 84)):
+    """Preprocesses a 210x160x3 frame to 84x84x1 grayscale
+
+    Arguments:
+        frame: The frame to process.  Must have values ranging from 0-255
+
+    Returns:
+        The processed frame
+    """
+    image = image.astype(np.uint8)  # cv2 requires np.uint8
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    image = image[34:34+160, :160]  # crop image
+    image = cv2.resize(image, shape, interpolation=cv2.INTER_NEAREST)
+    image = image.reshape((*shape, 1))
+
+    return image
